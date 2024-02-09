8950354db66ec5af8df68bacd9a171104081cc93
yahyoHakimov <hakiomvyahyo7@gmail.com>
2024-02-09 09:46:45 +0500

    Github Pipelines

---
+++ OpenAI.NET.Infrastructure.Build/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+Console.WriteLine("Hello, World!");
+var adotNetClient = new ADotNetClient();
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "OpenAI.NET Build",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2019,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Pulling Code",
+
+                },
+                new SetupDotNetTaskV1
+                {
+                    Name = "Installing .NET",
+
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "7.0.201",
+                        IncludePrerelease = true
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring .NET Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Building Solution"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Tests"
+                }
+            }
+        }
+    }
+};
+
+adotNetClient.SerializeAndWriteToFile(
+    githubPipeline,
+    "../../../.github/workflows/dotnet.yml"
+    );
+
